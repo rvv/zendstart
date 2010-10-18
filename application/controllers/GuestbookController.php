6cabac54898c33becfa261c0e0c8f87f9daf982d
rvv <9732697@gmail.com>
2010-10-19 00:31:48 +0400

    первый коммит всего проекта

---
+++ application/controllers/GuestbookController.php
@@ -0,0 +1,19 @@
+<?php
+
+class GuestbookController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        $guestbook = new Application_Model_GuestbookMapper();
+        $this->view->entries = $guestbook->fetchAll();
+    }
+
+
+}
+
